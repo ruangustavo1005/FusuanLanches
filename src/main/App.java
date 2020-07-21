b44557e8126c1e2a018d6a03ffd535ca2dd22134
ruanzada <ruan.gustavo.moretti10@gmail.com>
2020-07-20 22:12:11 -0300

    mandei a braba

--- src/main/App.java
+++ src/main/App.java
@@ -9,6 +9,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Teste");
         System.out.println("Corno");
+        System.out.println("o pai tá online");
     }
     
 }
