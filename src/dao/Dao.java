48d7807acdd03e80a485927ed61450f168dfb9cc
ruanzada <ruan.gustavo.moretti10@gmail.com>
2020-08-05 20:41:15 -0300

    Ajustes tela usuário

--- src/dao/Dao.java
+++ src/dao/Dao.java
@@ -10,7 +10,21 @@ public class Dao<Type> {
     
     private Type            model;
     private ArrayList<Type> lista;
-    
-    
-    
+
+    public Dao() {
+        
+    }
+
+    public Type getModel() {
+        return model;
+    }
+
+    public void setModel(Type model) {
+        this.model = model;
+    }
+
+    public ArrayList<Type> getLista() {
+        return lista;
+    }
+
 }
