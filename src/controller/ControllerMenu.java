63396031eededee4f4be8890c2da7bca81c7f179
ruanzada <ruan.gustavo.moretti10@gmail.com>
2020-08-09 13:53:56 -0300

    Logout

--- src/controller/ControllerMenu.java
+++ src/controller/ControllerMenu.java
@@ -2,8 +2,6 @@ package controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import view.ViewCadastroComanda;
-import view.ViewCadastroUsuario;
 import view.ViewIndex;
 
 /**
